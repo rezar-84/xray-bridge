88268ad6f292d901e7fd2dc41de493d313d59b76
Reza Rezaei <9523-rezarez@users.noreply.drupalcode.org>
2023-04-15 01:40:53 +0330

    Update vless.py

--- xray-caddy-cdn/vless.py
+++ xray-caddy-cdn/vless.py
@@ -1,24 +1,69 @@
 #!/usr/bin/python3
-
-import base64
 import json
-from pathlib import Path
+import re
+import base64
+import uuid
 
-path = Path(__file__).parent
 
-config_file = open(
-    str(path.joinpath('xray/config/config.json')), 'r', encoding='utf-8')
-config = json.load(config_file)
+def read_xray_config():
+    with open("xray/config/config.json", "r") as file:
+        config = json.load(file)
+    return config
 
-caddy = open(str(path.joinpath('caddy/Caddyfile')),
-             'r', encoding='utf-8').read()
 
-uuid = config['inbounds'][0]['settings']['clients'][0]['id']
-domain = caddy[:caddy.find(' {')]
+def read_caddyfile():
+    with open("caddy/Caddyfile", "r") as file:
+        caddyfile_contents = file.read()
+    return caddyfile_contents
 
-j = json.dumps({
-    "v": "2", "ps": domain, "add": domain, "port": "443", "id": uuid, "net": "ws", "type": "none",
-    "host": domain, "path": "/ws", "tls": "tls"
-})
 
-print("vless://" + base64.b64encode(j.encode('ascii')).decode('ascii'))
+def get_domain(caddyfile_contents):
+    domain = re.search(
+        r'^(http[s]?://)?([a-zA-Z0-9\-.]+)', caddyfile_contents, re.MULTILINE)
+    if domain:
+        return domain.group(2)
+    else:
+        raise ValueError("Domain not found in Caddyfile")
+
+
+def get_port_and_encryption(xray_config):
+    inbounds = xray_config.get("inbounds", [])
+    for inbound in inbounds:
+        protocol = inbound.get("protocol")
+        if protocol == "vless":
+            port = inbound.get("port")
+            settings = inbound.get("settings", {})
+            clients = settings.get("clients", [])
+            if clients:
+                client = clients[0]
+                encryption = client.get("encryption", "none")
+                return port, encryption
+    raise ValueError("VLESS protocol not found in Xray config")
+
+
+def create_vless_key(domain, port, uuid, encryption):
+    vless_url = f"vless://{domain}:{port}?id={uuid}&encryption={encryption}&type=tcp&security=tls"
+    return vless_url
+
+
+def base64_encode_vless_key(vless_key):
+    return base64.urlsafe_b64encode(vless_key.encode()).decode('utf-8')
+
+
+def main():
+    xray_config = read_xray_config()
+    caddyfile_contents = read_caddyfile()
+    domain = get_domain(caddyfile_contents)
+    port, encryption = get_port_and_encryption(xray_config)
+    uuid_key = str(uuid.uuid4())
+
+    vless_key = create_vless_key(domain, port, uuid_key, encryption)
+    base64_vless_key = base64_encode_vless_key(vless_key)
+
+    print("Domain:", domain)
+    print("VLESS Key (Normal):", vless_key)
+    print("VLESS Key (Base64 Encoded):", base64_vless_key)
+
+
+if __name__ == "__main__":
+    main()
