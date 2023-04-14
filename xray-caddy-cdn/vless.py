59ecad90b317efa3a0117b17e6afc02ac0cc0c8f
Reza Rezaei <9523-rezarez@users.noreply.drupalcode.org>
2023-04-15 01:28:45 +0330

    Update vless.py

--- xray-caddy-cdn/vless.py
+++ xray-caddy-cdn/vless.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import base64
 import json
 from pathlib import Path
@@ -15,20 +16,9 @@ caddy = open(str(path.joinpath('caddy/Caddyfile')),
 uuid = config['inbounds'][0]['settings']['clients'][0]['id']
 domain = caddy[:caddy.find(' {')]
 
-j = {
-    "v": "2",
-    "ps": domain,
-    "add": domain,
-    "port": "443",
-    "id": uuid,
-    "aid": "0",
-    "net": "ws",
-    "type": "none",
-    "host": "",
-    "path": "/ws",
-    "tls": "tls"
-}
+j = json.dumps({
+    "v": "2", "ps": domain, "add": domain, "port": "443", "id": uuid, "net": "ws", "type": "none",
+    "host": domain, "path": "/ws", "tls": "tls"
+})
 
-vless_link = "vless://" + \
-    base64.b64encode(json.dumps(j).encode('ascii')).decode('ascii')
-print(vless_link)
+print("vless://" + base64.b64encode(j.encode('ascii')).decode('ascii'))
